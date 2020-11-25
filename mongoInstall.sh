5585d86256f8d5a94944e1c103e3b24aa5fba028
Rahul Tarak <cryogenicplanet@gmail.com>
2020-11-25 12:34:08 +0530

    ✨ Mongo and Docker Install steps

---
+++ mongoInstall.sh
@@ -0,0 +1,13 @@
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+wget https://raw.githubusercontent.com/mongodb/mongo/master/debian/init.d
+
+sudo mv ./init.d /etc/init.d/mongod
+sudo chmod +x /etc/init.d/mongod
+sudo service mongod start
